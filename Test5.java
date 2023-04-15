0292c237311124345bb90522d0891b3708ee76f7
MaddurMeghana <130749519+MaddurMeghana@users.noreply.github.com>
2023-04-15 16:27:21 +0530

    Add files via upload

---
+++ Test5.java
@@ -0,0 +1,24 @@
+package com.day3;
+
+import java.util.Scanner;
+
+public class Test5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc=new Scanner (System.in);
+		
+		System.out.println("Enter Name of Person:");
+		String Pname=sc.nextLine();
+		
+        System.out.println("Age of Person:");
+        
+        int age=sc.nextInt();
+        
+        System.out.println("The name of person is:"+Pname+"\nThe age of person is:"+age);
+		
+		
+	}
+
+}
